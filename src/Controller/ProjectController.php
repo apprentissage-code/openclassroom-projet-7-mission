19d9f7048fdce2326e89362895f1f5459abe6f7b
Solene Baudic <solene.baudic@gmail.com>
2025-02-21 15:44:11 +0100

    Feat - create home page

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProjectController extends AbstractController
+{
+  #[Route('/', name: 'app_home')]
+  public function index(ProjectRepository $projectRepository): Response
+  {
+    $projects = $projectRepository->findAll();
+
+    return $this->render('project/index.html.twig', [
+      'projects' => $projects,
+    ]);
+  }
+}

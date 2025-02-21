456f5508ca27fb8cc7cf958a09f9c46709e386e3
Solene Baudic <solene.baudic@gmail.com>
2025-02-21 16:26:13 +0100

    Feat - create project form

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Form\ProjectFormType;
 use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +20,25 @@ final class ProjectController extends AbstractController
 
     return $this->render('project/index.html.twig', [
       'projects' => $projects,
+    ]);
+  }
+
+  #[Route('/project/add', name: 'app_new_project', methods: ['GET', 'POST'])]
+  public function new(Request $request, EntityManagerInterface $entityManager): Response
+  {
+    $project = new Project();
+    $form = $this->createForm(ProjectFormType::class, $project);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager->persist($project);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('app_home');
+    }
+
+    return $this->render('project/new.html.twig', [
+      'form' => $form,
     ]);
   }
 }
---
+++ src/Form/ProjectFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+use App\Entity\Project;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectFormType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('title', TextType::class, ['label' => 'Titre du projet'])
+      ->add('members', EntityType::class, [
+        'class' => Employee::class,
+        'choice_label' => 'lastname',
+        'multiple' => true,
+        'label' => 'Inviter des membres'
+      ])
+    ;
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Project::class,
+    ]);
+  }
+}

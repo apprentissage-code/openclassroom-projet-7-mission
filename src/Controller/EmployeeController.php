97dad8e7781d32a622bd373d710800128cfef060
Solene Baudic <solene.baudic@gmail.com>
2025-02-27 09:45:04 +0100

    Feat - implement form edit for employee

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Employee;
+use App\Form\EmployeeFormType;
 use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +21,29 @@ final class EmployeeController extends AbstractController
 
     return $this->render('employee/employees.html.twig', [
       'employees' => $employees,
+    ]);
+  }
+
+  #[Route('/employee/{id}/edit', name: 'app_employee_edit', requirements: ['id' => '\d+'])]
+  public function edit(int $id, EmployeeRepository $employeeRepository, Request $request, EntityManagerInterface $entityManager): Response
+  {
+    $employee = $employeeRepository->find($id);
+
+    if (!$employee) {
+      throw $this->createNotFoundException('Employé non trouvé.');
+    }
+
+    $form = $this->createForm(EmployeeFormType::class, $employee);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager->flush();
+
+      return $this->redirectToRoute('app_employees');
+    }
+
+    return $this->render('employee/edit.html.twig', [
+      'form' => $form,
     ]);
   }
 }
--- src/Form/EmployeeFormType.php
+++ src/Form/EmployeeFormType.php
@@ -6,33 +6,32 @@ use App\Entity\Employee;
 use App\Entity\Project;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EmployeeFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('mail')
-            ->add('EntryDate', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('status')
-            ->add('projects', EntityType::class, [
-                'class' => Project::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
-        ;
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('lastname', TextType::class, ['label' => 'Nom'])
+      ->add('firstname', TextType::class, ['label' => 'Prénom'])
+      ->add('mail', EmailType::class, ['label' => 'Email'])
+      ->add('EntryDate', DateTimeType::class, [
+        'widget' => 'single_text',
+        'label' => 'Date d\'entrée'
+      ])
+      ->add('status', TextType::class, ['label' => 'Statut'])
+    ;
+  }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Employee::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Employee::class,
+    ]);
+  }
 }
